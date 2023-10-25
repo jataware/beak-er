10792a5e7512f96b5ca36b462c6e5eeccb62fe1d
Five Grant <5@fivegrant.com>
2023-10-25 16:12:06 -0500

    Switch to JSON3

--- contexts/codesets/decapodes/julia-1.9/load_model.jl
+++ contexts/codesets/decapodes/julia-1.9/load_model.jl
@@ -1,5 +1,7 @@
-using ACSets, Decapodes
-import HTTP, JSON, DisplayAs
-_decapode = JSON.parse(String(HTTP.get("{{ model_url }}").body))["model"]
-{{ var_name|default("model") }} = parse_json_acset(SummationDecapode{String, String, String}, _decapode)
-Dict(["var_name" => "{{ var_name|default("model") }}"]) |> DisplayAs.unlimited # TODO: Fix 'Unable to parse result' false error
+using ACSets, Decapodes, SyntacticModels
+import HTTP, JSON3, DisplayAs
+# _decapode = JSON.parse(String(HTTP.get("{{ model_url }}").body))["model"]
+# {{ var_name|default("model") }} = parse_json_acset(SummationDecapode{String, String, String}, _decapode)
+# Dict(["var_name" => "{{ var_name|default("model") }}"]) |> DisplayAs.unlimited # TODO: Fix 'Unable to parse result' false error
+_amr = HTTP.get("{{ model_url }}").body |> JSON.parse ∘ String
+{{ var_name|default("model") }} = _amr |> Decapodes.SummationDecapode ∘ SyntacticModels.AMR.amr_to_expr
