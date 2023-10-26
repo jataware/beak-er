737e9a9881a4c057c8b8e864f96fc1e6857cf1e3
Five Grant <5@fivegrant.com>
2023-10-26 10:26:33 -0500

    Move to using SyntacticModels

--- contexts/codesets/decapodes/julia-1.9/load_model.jl
+++ contexts/codesets/decapodes/julia-1.9/load_model.jl
@@ -1,7 +1,5 @@
 using ACSets, Decapodes, SyntacticModels
 import HTTP, JSON3, DisplayAs
-# _decapode = JSON.parse(String(HTTP.get("{{ model_url }}").body))["model"]
-# {{ var_name|default("model") }} = parse_json_acset(SummationDecapode{String, String, String}, _decapode)
+_amr = JSON3.read(String(HTTP.get("{{ model_url }}").body), SyntacticModels.ASKEMDecapodes.ASKEMDecaExpr)
+{{ var_name|default("model") }} = Decapodes.SummationDecapode(_amr.model)
 # Dict(["var_name" => "{{ var_name|default("model") }}"]) |> DisplayAs.unlimited # TODO: Fix 'Unable to parse result' false error
-_amr = HTTP.get("{{ model_url }}").body |> JSON.parse ∘ String
-{{ var_name|default("model") }} = _amr |> Decapodes.SummationDecapode ∘ SyntacticModels.AMR.amr_to_expr
